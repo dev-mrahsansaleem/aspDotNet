1dc10d8dc12f8286b100858f2082e31203f0ffa8
Ahsan Saleem <ahsansaleemreal@gmail.com>
2019-11-28 20:12:06 +0500

    create a data entry form and display it
    create a basic data input form and add a gridview. the code will collect valid (as possible as it can) and then save it on a sql data base.
    similarly retrive all data from sql data base and display it on a grid view.

---
+++ First Web App (.net)/First Web App (.net)/dataEntry.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="dataEntry.aspx.cs" Inherits="First_Web_App__.net_.dataEntry" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Name:
+    
+        <asp:TextBox ID="tbName" runat="server"></asp:TextBox>
+    
+    </div>
+        <p>
+            father Name:
+            <asp:TextBox ID="tbFatherName" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            gender:
+            <asp:RadioButton ID="rbMale" runat="server" Text="Male" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="rbFemale" runat="server" Text="Female" />
+        </p>
+        <p>
+            date of birth:
+            <asp:TextBox ID="TBdateOfBirth" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            marital status: <asp:RadioButton ID="rbSingle" runat="server" Text="single" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="rbMarried" runat="server" Text="Married" />
+        </p>
+        <p>
+            Religin:
+            <asp:RadioButton ID="rbIslam" runat="server" Text="Islam" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="rbother" runat="server" Text="other" />
+        </p>
+        <p>
+            Qualification:&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:CheckBox ID="CBMatric" runat="server" Text="Matric" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:CheckBox ID="CBfsc" runat="server" Text="FSc." />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:CheckBox ID="CBBS_BSc" runat="server" Text="BS/BSc" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:CheckBox ID="CBother" runat="server" Text="Other" />
+        </p>
+        Address:<asp:TextBox ID="TBAddress" runat="server" Width="565px"></asp:TextBox>
+&nbsp;<p>
+            Phone Number: <asp:TextBox ID="TBphoneNo" runat="server" Width="194px"></asp:TextBox>
+        </p>
+        Email Id:
+        <asp:TextBox ID="TBemail" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="lblOut" runat="server"></asp:Label>
+        <p>
+            <asp:Button ID="btnReset" runat="server" Height="61px" OnClick="btnReset_Click" Text="Reset" Width="130px" />
+            <asp:Button ID="btnSubmit" runat="server" Height="60px" OnClick="btnSubmit_Click" Text="Submit" Width="130px" />
+        </p>
+        <asp:GridView ID="GridView1" runat="server" Height="374px" Width="660px">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ First Web App (.net)/First Web App (.net)/dataEntry.aspx.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace First_Web_App__.net_
+{
+    public partial class dataEntry : System.Web.UI.Page
+    {
+        private string name="";
+        private string fatherName="";
+        private string gender="";
+        private string dateOfBirth="";
+        private string maritalStatus="";
+        private string religin="";
+        private string qualification="";
+        private string address="";
+        private string phoneNo="";
+        private string email;
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-RFMGIDP;Initial Catalog=QuizDB;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            TBAddress.Text = TBdateOfBirth.Text = TBemail.Text = tbFatherName.Text = tbName.Text = TBphoneNo.Text = "";
+            rbMale.Checked = rbFemale.Checked = rbIslam.Checked = rbMale.Checked = rbMarried.Checked = rbother.Checked = rbSingle.Checked = CBBS_BSc.Checked = CBfsc.Checked = CBMatric.Checked = CBother.Checked = false;
+        }
+        public void disp()
+        {
+            
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Convert.ToInt32(tbName.Text);
+                tbName.Text = "must not be numaric";
+                tbName.BackColor = System.Drawing.Color.Red;
+            }
+            catch
+            {
+                name = tbName.Text;//string name axcepted
+                try
+                {
+                    Convert.ToInt32(tbFatherName.Text);
+                    tbFatherName.Text = "must not be numaric";
+                    tbFatherName.BackColor = System.Drawing.Color.Red;
+                }
+                catch
+                {
+                    fatherName = tbFatherName.Text;//string father name axcepted
+                    if (rbMale.Checked)
+                        gender = rbMale.Text;
+                    else if (rbFemale.Checked)
+                        gender = rbFemale.Text;
+                    //^^^^^^^^^^^^^^^^^^^^^^^ gender accepted
+                    dateOfBirth = TBdateOfBirth.Text;
+                    //^^^^^^^^^^^^^^^^^^^^^^  date of birth accepted
+                    if (rbSingle.Checked)
+                        maritalStatus = rbSingle.Text;
+                    else if (rbMarried.Checked)
+                        maritalStatus = rbMarried.Text;
+                    //^^^^^^^^^^^^^^^^^^^^^^^ marital status accepted
+                    if (rbIslam.Checked)
+                        religin = rbIslam.Text;
+                    else if (rbother.Checked)
+                        religin = rbother.Text;
+                    //^^^^^^^^^^^^^^^^^^^^^^^ relign accepted
+                    if (CBMatric.Checked)
+                        qualification = qualification + CBMatric.Text + " , ";
+                    if (CBfsc.Checked)
+                        qualification = qualification + CBfsc.Text + " , ";
+                    if (CBBS_BSc.Checked)
+                        qualification = qualification + CBBS_BSc.Text + " , ";
+                    if (CBother.Checked)
+                        qualification = qualification + CBother.Text + " , ";
+                    //^^^^^^^^^^^^^^^^^^^^^^^^^ qualification accepted
+                    address = TBAddress.Text;
+
+                    try
+                    {
+                        //Convert.ToInt64(TBphoneNo.Text);
+                        phoneNo = TBphoneNo.Text;//valid numeric phone number accepted
+
+                        email = TBemail.Text;//email accepted
+
+                        if(con.State==ConnectionState.Closed)
+                            con.Close();
+                        con.Open();
+                        SqlCommand cmd = new SqlCommand("Insert INTO [QuizDB].[dbo].[inputDataTable] ([name],[fatherName],[email],[phoneNo],[address],[gender],[religin],[dateOfBirth],[maritalStatus],[qualification]) VALUES ('" + name + "','" + fatherName + "','" + email + "','" + phoneNo + "','" + address + "','" + gender + "','" + religin + "','" + dateOfBirth + "','" + maritalStatus + "','" + qualification + "')", con);
+                        //SqlCommand cmd = new SqlCommand("Insert INTO [QuizDB].[dbo].[inputDataTable] ([name]) VALUES ('"+name+"')", con);
+                        cmd.ExecuteNonQuery();
+                        try
+                        {
+                            SqlCommand cmdd = new SqlCommand("Select * FROM [QuizDB].[dbo].[inputDataTable]", con);
+                            SqlDataReader dr = cmdd.ExecuteReader();
+                            GridView1.DataSource = dr;
+                            GridView1.DataBind();
+                        }
+                        catch
+                        {
+                            lblOut.Text = "No Record Found";
+                        }
+                        con.Close();
+                        //^^^^^^^^^^^^^^^^^ data enter in database
+
+                    }
+                    catch
+                    {
+                        TBphoneNo.Text = "phone number must be numaric";
+                        TBphoneNo.BackColor = System.Drawing.Color.Red;
+                    }
+                }
+            }
+        }
+    }
+}
